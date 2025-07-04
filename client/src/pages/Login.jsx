733ee295653cd1cffa2a1a744d17d883d7ccfdf8
kumkumrathore <rathorekumkum121@gmail.com>
2025-07-04 14:19:25 +0530

    : Added routing, login/register UI with ShadCN and Axios

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 const login = () => {
   return (
-    <div>
-      <h2 className='text-sky-500 text-2xl'>Login</h2>
+    <div className="max-w-md mx-auto mt-10 p-4 border rounded-xl">
+      <h2 className="text-xl font-bold mb-4">Login</h2>
+      <Input type="email" placeholder="Email" className="mb-4" />
+      <Input type="password" placeholder="Password" className="mb-4" />
+      <Button className="w-full">Login</Button>
     </div>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,15 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 const register = () => {
   return (
-    <div>
-      <h2 className='text-red-200 text-2xl'>register page</h2>
+     <div className="max-w-md mx-auto mt-10 p-4 border rounded-xl">
+      <h2 className="text-xl font-bold mb-4">Register</h2>
+      <Input type="text" placeholder="Name" className="mb-4" />
+      <Input type="email" placeholder="Email" className="mb-4" />
+      <Input type="password" placeholder="Password" className="mb-4" />
+      <Button className="w-full">Register</Button>
     </div>
   )
 }
